de4c63d13390f8b36972cbbc78f58a1736bc75f1
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-05-16 13:48:44 +0300

    Added test coverage

--- CleanEmonBackend/tests/test_lib/test_DBConnector.py
+++ CleanEmonBackend/tests/test_lib/test_DBConnector.py
@@ -2,6 +2,18 @@ import pytest
 
 from CleanEmonCore.models import EnergyData
 from CleanEmonBackend.lib.DBConnector import fetch_data
+from CleanEmonBackend.lib.DBConnector import send_data
+
+DUMMY_DATE = "2000-01-01"
+
+
+@pytest.fixture
+def energy_data():
+    return EnergyData(DUMMY_DATE, [
+        {"timestamp": 1, "power": 1, "temp": 1},
+        {"timestamp": 2, "power": 2, "temp": 2},
+        {"timestamp": 3, "power": 3, "temp": 3}
+    ])
 
 
 @pytest.mark.projectwise
@@ -9,3 +21,11 @@ def test_fetch_data():
     data = fetch_data("2022-05-01")
     assert data
     assert type(data) is EnergyData
+
+    data = fetch_data("2022-05-01", from_clean_db=True)
+    assert not data.energy_data
+
+
+@pytest.mark.projectwise
+def test_send_data(energy_data):
+    assert send_data(DUMMY_DATE, energy_data)
