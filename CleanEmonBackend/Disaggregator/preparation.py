6a010416af0ff995bf51547f99c4f7de2ca5e335
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-05-17 14:54:47 +0300

    Updated preparation module

--- CleanEmonBackend/Disaggregator/preparation.py
+++ CleanEmonBackend/Disaggregator/preparation.py
@@ -14,7 +14,47 @@ def reformat_timestamp(stamp: float) -> str:
     return dt.strftime("%Y-%m-%d %H:%M:%S+01:00")
 
 
-def to_continuous_time(df: pd.DataFrame) -> pd.DataFrame:
+def quantize_by_time(df: pd.DataFrame) -> pd.DataFrame:
+    """Accepts a dataframe indexed by non-continuous datetime objects and returns a fully continuous dataframe.
+
+    _Continuous_ refers to the series that has no gaps. Not to be confused with _continuous/discrete_ terms.
+
+    Example:
+    Say that INTERVAL=5. That means that every 5 seconds a new sample enters your dataframe. Ideally, by the end of the
+    day you would have a dataframe with 17280 sample. This would look like so:
+    -------Dataframe1------------
+    00:00:00    values_0
+    00:00:05    values_1
+    00:00:10    values_2
+    ...         ...
+    23:59:45    values_17277
+    23:59:50    values_17278
+    23:59:50    values_17279
+    -----------------------------
+
+    Note that the values range from 0 up to 17279 (17280 in total). This is a fully continuous day, because no
+    timeslots were missed.
+
+    However, it is quite possible that your sampling system is not precise enough and some samples would fall out of
+    that quantized pattern. The following dataframe is more likely to occur:
+    -------Dataframe2------------
+    00:00:03    values_0
+    00:00:08    values_1
+    00:00:12    values_2
+    ...         ...
+    23:59:45    values_17201
+    23:59:49    values_17202
+    23:59:57    values_17203
+    -----------------------------
+
+    Note that both indexes are not quantized (they don't repeat each 5 seconds) and the total number of samples is not
+    17280, as some of them are never captured.
+
+    This function would map Dataframe2 to Dataframe1. The workflow consists of those steps:
+        1. Round each timestamp to its closest quantum
+        2. If more than one original samples map to the same quantum, keep only one of them
+        3. Quantum with no value still get to stay in their position even they may be empty (no sample mapped to them)
+    """
     df.copy()
 
     # Copy some datetime details from the original dataframe
@@ -56,7 +96,7 @@ def energy_data_to_dataframe(data: EnergyData, timestamp_label: str = "timestamp
     df = df[~df.index.duplicated(keep="first")]
 
     # Generate a clean dataframe. All potentially empty time slots are filled
-    df = to_continuous_time(df)
+    df = quantize_by_time(df)
 
     # Reset index as expected by NILM-Inference-APIs
     df = df.reset_index()
