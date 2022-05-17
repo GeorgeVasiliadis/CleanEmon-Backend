4b0bd2b8d701237da1bcca590c62f57047bf5e9d
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-05-17 22:15:39 +0300

    Added test coverage for preparation

--- CleanEmonBackend/tests/test_Disaggregator/test_preparation.py
+++ CleanEmonBackend/tests/test_Disaggregator/test_preparation.py
@@ -1,23 +1,4 @@
-import os
-import json
-
-import pandas as pd
-import pytest
-
-from CleanEmonCore.models import EnergyData
-
 from CleanEmonBackend.Disaggregator.preparation import energy_data_to_dataframe
-
-
-@pytest.fixture
-def energy_data() -> EnergyData:
-    directory = os.path.dirname(__file__)
-    file = os.path.join(directory, "energy_data.json")
-
-    with open(file, "r") as fp:
-        data_dict = json.load(fp)
-
-    return EnergyData(data_dict["date"], data_dict["energy_data"])
 
 
 def test_energy_data_to_dataframe(energy_data):
