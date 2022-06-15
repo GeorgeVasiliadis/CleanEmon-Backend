b51db4d2bc120e4e2a9d5fab829894ba05e0d2c5
George Vasiliadis <geor.vasiliadis@gmail.com>
2022-06-15 14:38:07 +0300

    Cleaned app_factory.py

--- src/CleanEmonBackend/API/app_factory.py
+++ src/CleanEmonBackend/API/app_factory.py
@@ -1,3 +1,5 @@
+"""This defines the FastAPI boostrap function"""
+
 import datetime
 from typing import Optional
 
@@ -8,16 +10,18 @@ from fastapi.responses import FileResponse
 
 
 def create_app():
+    """Creates the FastAPI app"""
+
     from .API import get_data
     from .API import get_range_data
-    from .API import get_processed_kwh
+    from .API import get_date_consumption
     from .API import get_plot
 
-    from CleanEmonBackend.lib.exceptions import BadDateError
-    from CleanEmonBackend.lib.exceptions import BadDateRangeError
+    from ..lib.exceptions import BadDateError
+    from ..lib.exceptions import BadDateRangeError
 
-    from CleanEmonBackend.lib.validation import is_valid_date
-    from CleanEmonBackend.lib.validation import is_valid_date_range
+    from ..lib.validation import is_valid_date
+    from ..lib.validation import is_valid_date_range
 
     meta_tags = [
         {
@@ -49,15 +53,9 @@ def create_app():
 
     @app.get("/json/date/{date}", tags=["Views"])
     def get_json_date(date: str = None, clean: bool = False, from_cache: bool = True, sensors: Optional[str] = None):
-        """Returns the daily data for the supplied **{date}**. If {date} is omitted, then **{date}** is automatically
-        set to today's date.
-
-        If **to_date** parameter is used, then a range of daily data is returned starting from **{date}** up to
-        **to_date**
+        """Returns the daily data for the supplied **{date}**.
 
         - **{date}**: A date in YYYY-MM-DD format
-        - **to_date**: A date in YYYY-MM-DD format. If present, defines the inclusive end of date range for returned
-        data
         - **clean**: If set to True, requests an on-demand disaggregation and cleaning over the returned data. This is
         only useful when dealing with today's data
         - **from_cache**: If set to False, forces data to be fetched again from the central database. If set to True,
@@ -86,15 +84,10 @@ def create_app():
     @app.get("/json/range/{from_date}/{to_date}", tags=["Views"])
     def get_json_range(from_date: str, to_date: str, clean: bool = False, from_cache: bool = True,
                        sensors: Optional[str] = None):
-        """Returns the daily data for the supplied **{date}**. If {date} is omitted, then **{date}** is automatically
-        set to today's date.
+        """Returns the range data for the supplied range, from **{from_date}** to **{to_date}**.
 
-        If **to_date** parameter is used, then a range of daily data is returned starting from **{date}** up to
-        **to_date**
-
-        - **{date}**: A date in YYYY-MM-DD format
-        - **to_date**: A date in YYYY-MM-DD format. If present, defines the inclusive end of date range for returned
-        data
+        - **{from_date}**: A date in YYYY-MM-DD format
+        - **to_date**: A date in YYYY-MM-DD format. It should be chronologically greater or equal to **{from_date}**
         - **clean**: If set to True, requests an on-demand disaggregation and cleaning over the returned data. This is
         only useful when dealing with today's data
         - **from_cache**: If set to False, forces data to be fetched again from the central database. If set to True,
@@ -113,21 +106,55 @@ def create_app():
 
     @app.get("/plot/date/{date}", tags=["Experimental"])
     def get_plot_date(date: str = None, clean: bool = False, from_cache: bool = True, sensors: Optional[str] = None):
+        """Returns the plot of the specified data, as a JPEG image.
+
+        - **{date}**: A date in YYYY-MM-DD format
+        - **clean**: If set to True, requests an on-demand disaggregation and cleaning over the returned data. This is
+        only useful when dealing with today's data
+        - **from_cache**: If set to False, forces data to be fetched again from the central database. If set to True,
+        data will be looked up in cache and then, if they are not found, fetched from the central database.
+        - **sensors**: A comma (,) separated list of sensors to be returned. If present, only sensors defined in that
+        list will be returned
+        """
+
+        parsed_date: str
+
+        if date.lower() == "today":
+            parsed_date = datetime.date.today().isoformat()
+        elif date.lower() == "yesterday":
+            yesterday = datetime.date.today() - datetime.timedelta(days=1)
+            parsed_date = yesterday.isoformat()
+        elif is_valid_date(date):
+            parsed_date = date
+        else:
+            raise BadDateError(date)
+
         if sensors:
             sensors = sensors.split(',')
-        plot_path = get_plot(date, from_cache, sensors)
+
+        plot_path = get_plot(parsed_date, from_cache, sensors)
+
         return FileResponse(plot_path, media_type="image/jpeg")
 
     @app.get("/plot/range/{from_date}/{to_date}", tags=["Experimental"])
     def get_plot_range(from_date: str, to_date: str, clean: bool = False, from_cache: bool = True,
                        sensors: Optional[str] = None):
+        """Under construction :)"""
         return JSONResponse(
             status_code=501,
             content={"message": "This feature is currently not implemented"}
         )
 
-    @app.get("/json/date/{date}/kwh", tags=["Views"])
-    def get_processed_json_date_kwh(date: str = None, clean: bool = False, from_cache: bool = True):
+    @app.get("/json/date/{date}/consumption", tags=["Views"])
+    def get_json_date_consumption(date: str = None, clean: bool = False, from_cache: bool = True):
+        """Returns the power consumption for the given date.
+
+        - **{date}**: A date in YYYY-MM-DD format
+        - **clean**: If set to True, requests an on-demand disaggregation and cleaning over the returned data. This is
+        only useful when dealing with today's data
+        - **from_cache**: If set to False, forces data to be fetched again from the central database. If set to True,
+        data will be looked up in cache and then, if they are not found, fetched from the central database.
+        """
 
         parsed_date: str
 
