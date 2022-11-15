087ae703ab78c3323cdd74cb0780e119f4f724c1
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2022-11-15 19:39:28 +0200

    Reformat according to PEP 8: E305

--- src/CleanEmonBackend/API/service.py
+++ src/CleanEmonBackend/API/service.py
@@ -6,6 +6,7 @@ import uvicorn
 def run():
     uvicorn.run("CleanEmonBackend.API:api", reload=True)
 
+
 # Add this to help testing and debugging quickly through an IDE. TODO: Remove in release
 if __name__ == '__main__':
     run()
