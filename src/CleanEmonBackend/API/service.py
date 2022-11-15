cc1f16c2921cc26068b5ada2959ed922934faf49
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2022-11-15 19:37:18 +0200

    Add this to help testing and debugging quickly through an IDE.

--- src/CleanEmonBackend/API/service.py
+++ src/CleanEmonBackend/API/service.py
@@ -5,3 +5,7 @@ import uvicorn
 
 def run():
     uvicorn.run("CleanEmonBackend.API:api", reload=True)
+
+# Add this for to help test thing quick through an IDE TODO: Remove in release
+if __name__ == '__main__':
+    run()
