569847f842da4305bde9ec0955487de99cf8ee59
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2022-12-08 21:29:32 +0200

    Refactor code to reflect changes on CleanEmon Core

--- src/CleanEmonBackend/Devices/devices.py
+++ src/CleanEmonBackend/Devices/devices.py
@@ -2,15 +2,15 @@
 import json
 
 from CleanEmonCore import DEVICES_FILE
-from CleanEmonCore.json_utils.json_reader import read_json_file
-from CleanEmonCore.json_utils.json_schema_validator import verify_json
+from CleanEmonCore.json_utils.schemas import schema_devices
+from CleanEmonCore.json_utils.json_utils import verify_json, read_json_file
 
 
 class Devices:
     """Devices class used to just to check to parse the JSON file under the .CleanEmon directory."""
 
     def __init__(self):
-        self.devices = verify_json(read_json_file(DEVICES_FILE))['devices']
+        self.devices = verify_json(read_json_file(DEVICES_FILE), schema_devices)['devices']
 
     def get_devices(self):
         return self.devices
